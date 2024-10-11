69bd8f6ce3bdd48805f6e8065c0fddbd6dc5cac1
UT01490 <UT01490@UNICOMTIC30>
2024-10-11 16:06:53 +0530

    10 and 11 quetion commit

---
+++ CourseManagementSystem_v2/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseManagementSystem_v2
+{
+    internal class Course
+    {
+        private string CourseId { get; set; }
+        private string Title { get; set; }
+        private string Duration { get; set; }
+        private decimal Price { get; set; }
+    }
+}
